9f13271f71f51e101d2fb78fb8acc78160ae4dac
rupam sahriar <rupamsahriar@gmail.com>
2020-06-14 05:42:34 +0600

    disble and enable button [chat.js]

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,21 +9,36 @@ const socket = io()
 //     socket.emit('incriment')
 // })
 
+// #Element
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const sendLocationButton = document.querySelector('#send-location')
 socket.on('message', (text)=>{
     console.log(text)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e)=>{
+$messageForm.addEventListener('submit', (e)=>{
     e.preventDefault()
 
+    // disabling button
+$messageFormButton.setAttribute('disabled','disabled')
+    
     // const message=document.querySelector('input').value
     const message= e.target.elements.message.value
 
-    // socket.emit('sendMessage', message) //AS USUAL
+    //AS USUAL
+    // socket.emit('sendMessage', message) 
+
     // Using Acknowledgement
     socket.emit('sendMessage', message, (error)=>{
-      //  console.log('The Message ', error)
-      if(error) return console.log(error)
+      // enabling button
+      $messageFormButton.removeAttribute('disabled')
+      $messageFormInput.value=''
+      $messageFormInput.focus()
+
+        //  console.log('The Message ', error)
+        if(error) return console.log(error)
 
       console.log('Message Deliverd')
     })
@@ -43,10 +58,13 @@ document.querySelector('#message-form').addEventListener('submit', (e)=>{
 //     })
 //})
 
-document.querySelector('#send-location').addEventListener('click', ()=>{
+sendLocationButton.addEventListener('click', ()=>{
     if(!navigator.geolocation){
         return alert('Geolocation Not Supported in your browser')
     }
+    
+    // disable button
+    sendLocationButton.setAttribute('disabled','disabled')
 
     navigator.geolocation.getCurrentPosition((position)=>{
         
@@ -54,6 +72,8 @@ document.querySelector('#send-location').addEventListener('click', ()=>{
             latitude : position.coords.latitude,
             longitude : position.coords.longitude
         },() =>{
+            //enable button
+            sendLocationButton.removeAttribute('disabled')
             console.log('Location Shared')
         })
     })
